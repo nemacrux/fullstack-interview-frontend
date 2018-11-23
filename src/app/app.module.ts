ce4e924758d42a3be2b4bc3bc84b76f23eb83cea
Nestor Marin <nestor.mc@gmail.com>
2018-11-23 14:13:21 -0500

    Add Covalent UI Platform / Angular-Material
    * update styles/theme
    * update dependencies

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,16 +1,42 @@
-import { BrowserModule } from '@angular/platform-browser';
-import { NgModule } from '@angular/core';
+import { BrowserModule } from "@angular/platform-browser";
+import { BrowserAnimationsModule } from "@angular/platform-browser/animations";
+import { NgModule } from "@angular/core";
 
-import { AppComponent } from './app.component';
+import { CovalentLayoutModule } from "@covalent/core/layout";
+import { CovalentCommonModule } from "@covalent/core/common";
+import { CovalentMediaModule } from "@covalent/core/media";
+import { CovalentLoadingModule } from "@covalent/core/loading";
+
+import { MatButtonModule } from "@angular/material/button";
+import { MatCardModule } from "@angular/material/card";
+import { MatIconModule } from "@angular/material/icon";
+import { MatListModule } from "@angular/material/list";
+import { MatDividerModule } from "@angular/material/divider";
+import { MatInputModule } from "@angular/material/input";
+import { MatToolbarModule } from "@angular/material/toolbar";
+
+import { AppComponent } from "./app.component";
 
 @NgModule({
-  declarations: [
-    AppComponent
-  ],
-  imports: [
-    BrowserModule
-  ],
-  providers: [],
-  bootstrap: [AppComponent]
+    declarations: [AppComponent],
+    imports: [
+        BrowserModule,
+        BrowserAnimationsModule,
+        // covalent modules
+        CovalentCommonModule,
+        CovalentLayoutModule,
+        CovalentLoadingModule,
+        CovalentMediaModule,
+        // material modules
+        MatButtonModule,
+        MatCardModule,
+        MatIconModule,
+        MatListModule,
+        MatDividerModule,
+        MatInputModule,
+        MatToolbarModule
+    ],
+    providers: [],
+    bootstrap: [AppComponent]
 })
-export class AppModule { }
+export class AppModule {}
